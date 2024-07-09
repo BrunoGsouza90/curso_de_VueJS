125d10a7fd43d50028d26401910facc52bcaf69d
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-09 18:00:15 -0300

    Criação do Curso de VueJs Tiago Mattos, e Aulas 1,2,3

---
+++ Curso de VueJS Tiago Mattos/app/src/App.vue
@@ -0,0 +1,36 @@
+<!-- Nosso site será renderizado nesse arquivo. -->
+
+<template>
+  
+  <TheHeader v-if="showHeader"></TheHeader>
+
+  <div v-show="ShowName">
+    <p>Nome: {{ name }}</p>
+    <p>Sobrenome: {{ sobrenome }}</p>
+  </div>
+
+  <div v-if="AcessLevel === 'Admin' || AcessLevel === 'Bruno'">Usuário Admin</div>
+  <div v-else-if="AcessLevel === 'Marketing'">Usuário Marketing</div>
+  <div v-else>Usuário Normal</div>
+
+</template>
+
+<script setup>
+
+  import TheHeader from './components/TheHeader.vue';
+
+  let showHeader = true
+  let name = 'Bruno.'
+  let sobrenome = 'Gonçalves de Souza.'
+  let ShowName = true
+  let AcessLevel = 'Bruno'
+
+</script>
+
+<style>
+
+  body{
+    text-align: center;
+  }
+
+</style>
---
+++ Curso de VueJS Tiago Mattos/app/src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<!-- Área HTML do Vue.js. -->
+<template>
+    
+    <header class="header">
+        TheHeader
+    </header>
+
+</template>
+
+<!-- Área de Script do Vue.js. -->
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<!-- Área de Style do Vue.js .-->
+<style>
+
+    .header {
+       background: rgb(25, 175, 25);
+       color: white;
+    }
+
+</style>
